3c8a83245cda76534af874237bfb7129a000d37b
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2025-03-19 17:39:57 +0100

    feat: move to starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,36 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeBlack from 'starlight-theme-black'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [
+		starlight({
+			title: 'juanoa',
+			social: {
+				github: 'https://github.com/juanoa',
+				'x.com': 'https://x.com/juanoa',
+				linkedin: 'https://linkedin.com/in/juanoa'
+			},
+			sidebar: [
+				{
+					label: 'About',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'It\'s Juan', slug: 'about/me' },
+					],
+				},
+			],
+			plugins: [
+				starlightThemeBlack({
+					navLinks: [{
+						label: 'Docs',
+						link: '/getting-started',
+					}],
+					footerText: 'From Coruña to the world'
+				})
+			],
+		}),
+	],
 });
