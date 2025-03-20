6b0df7111190cdb980068595f346cb3c3e9db975
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2025-03-20 11:31:47 +0100

    feat: move to starlight (#1)
    * feat: move to starlight
    
    * fix: update navigation labels and links to reflect personal branding
    
    * feat: restructure documentation and update content to Spanish
    
    * feat: add Frontend handbook section and contributing guidelines
    
    * feat: add slugs to documentation pages for improved navigation
    
    * feat: update first steps documentation with new links and Frontend Handbook section
    
    * feat: add 'projects' section to navigation and create 'Reactioning' documentation page
    
    * feat: update favicon
    
    * feat: add section on the purpose of public sharing in 'first steps' documentation
    
    * feat: rename section header from '¿Cómo se estructura esta web?' to 'Estructura' in first steps documentation
    
    * feat: add 'Proyectos' link card to first steps documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,45 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeBlack from 'starlight-theme-black'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [
+		starlight({
+			title: 'juanoa',
+			favicon: '/favicon.png',
+			social: {
+				github: 'https://github.com/juanoa',
+				'x.com': 'https://x.com/juanoa',
+				linkedin: 'https://linkedin.com/in/juanoa'
+			},
+			defaultLocale: 'root',
+			sidebar: [{
+				label: 'Empieza aquí',
+				autogenerate: { directory: 'getting-started' },
+			},{
+				label: 'About',
+				autogenerate: { directory: 'about' },
+			},{
+				label: 'Proyectos',
+				autogenerate: { directory: 'projects' },
+			},{
+				label: 'Frontend handbook',
+				autogenerate: { directory: 'frontend-handbook' },
+			}],
+			locales: {
+				root: {
+					label: 'Spanish',
+					lang: 'es',
+				},
+			},
+			plugins: [
+				starlightThemeBlack({
+					navLinks: [],
+					footerText: 'From Coruña to the world'
+				})
+			],
+		}),
+	],
 });
