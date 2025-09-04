0598a70800ee0b1676e11d38da15e263fbf30931
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2025-09-04 12:55:48 +0200

    feat: generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.juanoa.com',
 	integrations: [starlight({
 		title: 'juanoa',
 		favicon: '/favicon.png',
